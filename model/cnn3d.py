cc4776769a39d3bd5e4a9f9c2e5e5ef335f15249
bmeatayi <bmeatayi@gmail.com>
2018-01-11 14:49:09 +0100

    new branch test

--- model/cnn3d.py
+++ model/cnn3d.py
@@ -1,9 +1,9 @@
-"""3dCNN"""
+"""CNN3d"""
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torchvision as tv
 
 
-class 3dCNN(nn.Module):
+class cnn3d(nn.Module):
 	def __init__(self, ...):
